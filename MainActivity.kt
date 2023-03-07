a50863d36cb225198f40e0b1f6b19b51eb29292d
blatnoa <noahblattner04@gmail.com>
2023-03-07 10:30:54 +0100

    Récuperation et affichage des données lu par le scanneur Sunmi

--- MainActivity.kt
+++ MainActivity.kt
@@ -11,9 +11,43 @@ import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
 
+    private val ACTION_STRING = "com.sunmi.scanner.ACTION_DATA_CODE_RECEIVED"
+
+    private lateinit var scanText: TextView
+
+    private lateinit var receiver: BroadcastReceiver
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        // Find the components
+        scanText = findViewById(R.id.scannerText)
+
+        // Create the broadcast receiver
+        receiver = object : BroadcastReceiver() {
+            override fun onReceive(context: Context, intent: Intent) {
+                // Get the data from the intent
+                val code = intent.getStringExtra("data")
+
+                if (code != null && !code.isEmpty()) {
+                    // Display the data
+                    scanText.text = code
+                    Toast.makeText(context, code, Toast.LENGTH_SHORT).show()
+                }
+            }
+        }
+
+        registerScannerReceiver(receiver)
+    }
+
+    /**
+     * Register the broadcast receiver
+     * @param receiver the broadcast receiver
+     */
+    private fun registerScannerReceiver(receiver: BroadcastReceiver) {
+        val filter = IntentFilter()
+        filter.addAction(ACTION_STRING)
+        registerReceiver(receiver, filter)
     }
 }
