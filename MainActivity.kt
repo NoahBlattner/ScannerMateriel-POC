4d63b8a0cd5956517b1277a5052d8ec047d52356
blatnoa <noahblattner04@gmail.com>
2023-03-07 09:28:25 +0100

    Initial commit

---
+++ MainActivity.kt
@@ -0,0 +1,19 @@
+package com.divtec.blatnoa.scannermateriel_poc
+
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+import android.content.IntentFilter
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.TextView
+import android.widget.Toast
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+    }
+}
